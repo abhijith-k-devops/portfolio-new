d08b012a70d06524f6cf534f83bb178d27c5c706
akottayi <v-akottayi@expediagroup.com>
2026-02-05 14:10:58 +0530

    intro change part 2

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -36,9 +36,9 @@ const Introduction = () => {
             </span>
           </p>
           <p className="text-xs xxs:text-lg lg:text-[18px] my-6">
-            I'm a Senior Android Developer based in
-            Kerala, India. I strives to build immersive and beautiful web
-            applications through carefully crafted code and user-centric design.
+            I'm an Experienced Android developer based in Kerala, India. I'm specializing in building scalable,
+             high-performance mobile applications. Proven track record of delivering enterprise
+              and consumer-facing solutions with modern architecture patterns and best practices.
           </p>
           <p className="text-center lg:text-start">
             <a
