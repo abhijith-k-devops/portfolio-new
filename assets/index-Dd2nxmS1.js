4e332ce17e5302a0a8d694e41111faf2a7519ee7
akottayi <v-akottayi@expediagroup.com>
2026-02-08 19:27:24 +0530

    chore(deploy): update assets for Poppins [skip ci]

--- assets/index-Dd2nxmS1.js
+++ assets/index-Dd2nxmS1.js
@@ -1,4 +1,4 @@
-import{g as bn,R as Te}from"./index-Dc31HnX6.js";/*!
+import{g as bn,R as Te}from"./index-f5JxkYon.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
